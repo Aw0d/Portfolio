261cf2cc5cb8c5cc36d398b719042331cd6b63ac
Aw0d <awod2004@yahoo.com>
2025-07-01 22:40:49 +0200

    Update aside

--- src/components/InfoAside.vue
+++ src/components/InfoAside.vue
@@ -1,18 +1,77 @@
 <template>
   <aside id="info-aside">
+    <!-- Photo de profil -->
     <figure>
-      <img class="photo-profil" src="/img/photo.png" alt="Photo de profil" />
+      <img
+        class="photo-profil"
+        src="/img/photo.png"
+        alt="Photo de profil de Mathieu Camus"
+      />
     </figure>
+
+    <!-- Nom -->
     <h2>Camus Mathieu</h2>
+
+    <!-- Infos principales -->
     <ul class="infos">
       <li>
-        <span class="emojis">&#128100;</span>Étudiant en troisième année de BUT informatique
+        <span class="emojis" aria-hidden="true">&#128100;</span>
+        Étudiant en informatique
       </li>
+
+      <!-- Réseaux sociaux -->
       <li class="reseaux">
         <ul>
+          <!-- GitHub -->
           <li>
-            <a href="https://github.com/Aw0d" target="_blank" rel="noopener">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 256 256"><g fill="#ffffff"><path d="M128 12C62 12 10 64 10 128c0 51 33 94 78 109 6 1 8-3 8-6v-21c-32 7-39-15-39-15-5-13-13-16-13-16-10-7 1-7 1-7 11 1 17 11 17 11 10 17 26 12 32 9 1-7 4-12 8-15-26-3-53-13-53-57 0-13 5-23 11-32-1-3-5-15 1-31 0 0 9-3 33 12 9-3 19-4 29-4s20 1 29 4c24-15 33-12 33-12 6 16 2 28 1 31 6 9 11 19 11 32 0 44-27 54-53 57 4 4 8 12 8 23v34c0 3 2 7 8 6 45-15 78-58 78-109C246 64 194 12 128 12z"/></g></svg>
+            <a
+              href="https://github.com/Aw0d"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Profil GitHub de Mathieu Camus"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 256 256"
+                role="img"
+                aria-hidden="true"
+              >
+                <g fill="#ffffff">
+                  <path
+                    d="M128 12C62 12 10 64 10 128c0 51 33 94 78 109 6 1 8-3 8-6v-21c-32 7-39-15-39-15-5-13-13-16-13-16-10-7 1-7 1-7 11 1 17 11 17 11 10 17 26 12 32 9 1-7 4-12 8-15-26-3-53-13-53-57 0-13 5-23 11-32-1-3-5-15 1-31 0 0 9-3 33 12 9-3 19-4 29-4s20 1 29 4c24-15 33-12 33-12 6 16 2 28 1 31 6 9 11 19 11 32 0 44-27 54-53 57 4 4 8 12 8 23v34c0 3 2 7 8 6 45-15 78-58 78-109C246 64 194 12 128 12z"
+                  />
+                </g>
+              </svg>
+            </a>
+          </li>
+
+          <!-- LinkedIn -->
+          <li>
+            <a
+              href="https://www.linkedin.com/in/mathieu-camus-82b304364/"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Profil LinkedIn de Mathieu Camus"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="#ffffff"
+                role="img"
+                aria-hidden="true"
+              >
+                <path
+                  d="M19 0h-14c-2.76 0-5 2.24-5 5v14c0 2.76 2.24 5 5 5h14
+                     c2.76 0 5-2.24 5-5v-14c0-2.76-2.24-5-5-5zm-11.75 20h-3.5v-11h3.5v11zm-1.75-12.28c-1.12
+                     0-2.02-.91-2.02-2.03s.9-2.02 2.02-2.02c1.12 0 2.02.9 2.02 2.02s-.9 2.03-2.02 2.03zm13.5
+                     12.28h-3.5v-5.5c0-1.33-.03-3.05-1.86-3.05-1.86 0-2.15 1.45-2.15 2.95v5.6h-3.5v-11h3.36v1.5h.05c.47-.89
+                     1.62-1.83 3.33-1.83 3.56 0 4.22 2.34 4.22 5.38v6.95z"
+                />
+              </svg>
             </a>
           </li>
         </ul>
